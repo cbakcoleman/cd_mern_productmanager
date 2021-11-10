a20c049443310f2340d64f5473761b88f1219c11
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-09 19:36:43 -0800

    create model schema

---
+++ server/models/product.models.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ProductScheuma = new mongoose.Schema({
+    title : {
+        type : String,
+        require : [true, "Product {PATH} is required"],
+        minlength : [3, "Product {PATH} must be at least 3 characters long"]
+    },
+    price : {
+        type : Number,
+        require : [true, "Product {PATH} is required"],
+        min : [0.99, "Product {PATH} can not be less that $0.99"]
+    },
+    description : {
+        type : String,
+        require : [true, "Product {PATH} is required"],
+        minlength : [3, "Product {PATH} must be at least 3 characters long"]
+    },
+    isFavorited : {
+        type : Boolean,
+        default : false
+    }
+}, {timestamps: true})
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
