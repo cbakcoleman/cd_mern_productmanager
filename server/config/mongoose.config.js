bef9ac03302edc9e20f7674a3019eabbadf259f9
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-09 19:04:21 -0800

    connect to db

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+module.exports = (DB) => {
+    mongoose.connect(`mongodb://localhost/${DB}`)
+        .then( () => console.log(`Connected to the ${DB} database`))
+        .catch( err => console.log(`Unable to connect to the ${DB} database`))
+}
--- server/server.js
+++ server/server.js
@@ -1,2 +1,16 @@
+// 2. SET UP SERVER
 const express = require("express");
 const cors = require("cors");
+const app = express();
+const PORT = 8000;
+const DB = "product_manager_DB";
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json(), express.urlencoded({extended: true}));
+
+// DATABASE CONNECTION LINK 
+require("./config/mongoose.config")(DB);
+
+// START THE SERVER
+app.listen(PORT, () => console.log(`Server is up on port: ${PORT}`))
